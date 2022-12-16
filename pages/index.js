591e2fbfbede0d7c7f3b77e07b1c2ff1f9a999ed
Febri Milansyah <febrimilan00@gmail.com>
2022-12-16 19:33:58 +0700

    Integrate with react-notifications

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import React from 'react'
-import {NotificationContainer} from 'react-notifications'
+import { NotificationContainer } from 'react-notifications'
 
 const inter = Inter({ subsets: ['latin'] })
 
