0916fc20844282da6fd400bf80a2ced34b55abf4
Febri Milansyah <febrimilan00@gmail.com>
2023-01-12 12:01:31 +0700

    Notification List

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import 'react-notifications/lib/notifications.css'
 import { MagicBellProvider } from '@magicbell/magicbell-react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }) {
   return (
     <MagicBellProvider apiKey={process.env.MAGICBELL_API_KEY} userEmail={process.env.MAGICBELL_USER_EMAIL}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </MagicBellProvider>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,60 @@
 import React from 'react'
 import { NotificationContainer, NotificationManager } from 'react-notifications'
-import { useMagicBellEvent } from '@magicbell/magicbell-react'
+import { useMagicBellEvent, useNotifications } from '@magicbell/magicbell-react'
+import {
+  Badge,
+  Box,
+  Heading,
+  SimpleGrid,
+  Text,
+  Container,
+  Center,
+} from "@chakra-ui/react"
+import { FaTrash } from "react-icons/fa"
 
 const Home = () => {
+  const store = useNotifications()
   const showPushNotification = (notification) => NotificationManager.info(notification?.content, notification?.title)
   useMagicBellEvent('notifications.new', showPushNotification)
   return (
     <>
+      <Container maxW="7xl">
+        <Center>
+          <Box w="320px">
+            <Heading as="h1" fontSize="xl" mb={5}>
+              Notifications
+            </Heading>
+            <SimpleGrid columns={{ base: 1 }} spacing={8}>
+              {store && store.notifications.map((notification, index) => (
+                <Box
+                  key={index}
+                  p={3}
+                  borderWidth="1px"
+                >
+                  <Heading as="h3" fontSize="xl">
+                    {notification.title}{" "}
+                    <Badge
+                      color="red.500"
+                      bg="inherit"
+                      transition="0.2s"
+                      _hover={{
+                        bg: "inherit",
+                        transform: "scale(1.2)",
+                      }}
+                      float="right"
+                      size="xs"
+                      onClick={() => console.log('delete notification')}
+                    >
+                      <FaTrash />
+                    </Badge>
+                  </Heading>
+                  <Text>{notification.content}</Text>
+                </Box>
+              ))}
+            </SimpleGrid>
+          </Box>
+        </Center>
+      </Container>
       <NotificationContainer />
     </>
   )
